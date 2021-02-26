22521b98afc0c2412c594914f2bdae8731d137a4
Nao <naonao1814naonao@gmail.com>
2021-02-26 19:48:43 +0900

    get_categories functionの追加
    サイドのカテゴリー

--- ecom/resources/functions.php
+++ ecom/resources/functions.php
@@ -63,6 +63,25 @@ echo $product;
 }
 
 
+function get_categories(){
+
+$query = query("SELECT * FROM categories");
+confirm($query);
+
+while($row = fetch_array($query)){
+  
+$category_links = <<<DELIMETER
+
+<a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>
+
+DELIMETER;
+
+echo $category_links;
+
+}
+}
+
 
 
 ?>
+
--- ecom/resources/templates/front/side_nav.php
+++ ecom/resources/templates/front/side_nav.php
@@ -4,17 +4,9 @@
 
        <?php
 
-        $query = "SELECT * FROM categories";
-        $send_query = mysqli_query($connection,$query);
-        
-        if(!$send_query){
-          die("QUERY FAILED" . mysqli_error($connection));
-        }
-        while($row = mysqli_fetch_array($send_query)){
-        
-         echo "<a href='' class='list-group-item'>{$row['cat_title']}</a>";
+       get_categories();
 
-        }
+        
       ?>
 
 
