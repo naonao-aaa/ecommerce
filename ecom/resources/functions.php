feabb6076e2f5608f64449126eb98c20f6c56408
Nao <naonao1814naonao@gmail.com>
2021-02-26 21:13:07 +0900

    製品ページのセットアップ
    3-19

--- ecom/resources/functions.php
+++ ecom/resources/functions.php
@@ -28,6 +28,9 @@ function fetch_array($result){
 return mysqli_fetch_array($result);
 }
 
+
+/********************FRONT END FUNCTIONS**********************/
+
 //get products
 
 function get_products(){
@@ -44,7 +47,7 @@ $product = <<<DELIMETER
     <a href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
     <div class="caption">
         <h4 class="pull-right">&#36;{$row['product_price']}</h4>
-        <h4><a href="product.html">{$row['product_title']}</a>
+        <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
         </h4>
         <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
         <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to cart</a>
@@ -81,6 +84,8 @@ echo $category_links;
 }
 }
 
+
+/********************BACK END FUNCTIONS**********************/
 
 
 ?>
