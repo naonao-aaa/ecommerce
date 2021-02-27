c6a537efcc6ac3e80a341e0a63f713a0cf8fd352
Nao <naonao1814naonao@gmail.com>
2021-02-27 11:15:13 +0900

    製品ページの更新
    価格表示

--- ecom/public/item.php
+++ ecom/public/item.php
@@ -8,6 +8,16 @@
 
        <?php include(TEMPLATE_FRONT .DS. "side_nav.php")?>
 
+<?php
+
+$query = query("SELECT *FROM products WHERE product_id =" . escape_string($_GET['id'])."  ");  
+confirm($query);
+
+while($row=fetch_array($query)):
+
+
+?>
+
 <div class="col-md-9">
 
 <!--Row For Image and Short Description-->
@@ -27,7 +37,7 @@
     <div class="caption-full">
         <h4><a href="#">Javascript Course</a> </h4>
         <hr>
-        <h4 class="">$24.99</h4>
+        <h4 class=""><?php echo "&#36;" . $row['product_price']; ?></h4>
 
     <div class="ratings">
      
@@ -192,7 +202,9 @@
 
 
 
-</div>
+</div><!-- col-md-9 ends here -->
+
+<?php endwhile; ?>
 
 </div>
     <!-- /.container -->
