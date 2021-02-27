2a89f95ab4ca3218575853466926a24050ca3189
Nao <naonao1814naonao@gmail.com>
2021-02-27 19:10:15 +0900

    ログインページの作成
    login_user functionの追加

--- ecom/resources/functions.php
+++ ecom/resources/functions.php
@@ -146,6 +146,30 @@ echo $product;
 
 }
 
+
+function login_user(){
+
+if(isset($_POST['submit'])){
+
+$username = escape_string($_POST['username']);
+$password = escape_string($_POST['password']);
+
+$query = query("SELECT * FROM users WHERE username = '{$username}' AND password = '{$password}'");
+confirm($query);
+
+if(mysqli_num_rows($query) == 0){
+
+redirect("login.php");
+
+}else{
+
+redirect("admin");  
+
+}
+
+}
+
+}
   
 /********************BACK END FUNCTIONS**********************/
 
