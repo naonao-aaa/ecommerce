4fcc01f60544a035f036d095624cc4bcb6e6c1cc
Nao <naonao1814naonao@gmail.com>
2021-02-27 12:16:45 +0900

    製品ページの更新
    画像、製品名、短い説明文、説明文

--- ecom/public/item.php
+++ ecom/public/item.php
@@ -25,7 +25,7 @@ while($row=fetch_array($query)):
 <div class="row">
 
     <div class="col-md-7">
-       <img class="img-responsive" src="http://placehold.it/700x600" alt="">
+       <img class="img-responsive" src="<?php echo $row['product_image'];?>" alt="">
 
     </div>
 
@@ -35,7 +35,7 @@ while($row=fetch_array($query)):
          
 
     <div class="caption-full">
-        <h4><a href="#">Javascript Course</a> </h4>
+        <h4><a href="#"><?php echo $row['product_title'];?></a> </h4>
         <hr>
         <h4 class=""><?php echo "&#36;" . $row['product_price']; ?></h4>
 
@@ -51,7 +51,7 @@ while($row=fetch_array($query)):
         </p>
     </div>
           
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <p><?php echo $row['short_desc'];?></p>
 
    
     <form action="">
@@ -92,13 +92,7 @@ while($row=fetch_array($query)):
 
 <p></p>
            
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-
+<p><?php echo $row['product_description'];?></p> 
     </div>
     <div role="tabpanel" class="tab-pane" id="profile">
 
