d090d6e356c833f8a54bbeae60122207f4117aba
Nao <naonao1814naonao@gmail.com>
2021-02-28 15:34:39 +0900

    カートページの作成

---
+++ ecom/public/cart.php
@@ -0,0 +1,38 @@
+<?php require_once("../resources/config.php"); ?>
+
+<?php
+
+if(isset($_GET['add'])){
+
+ $query = query("SELECT * FROM products WHERE product_id=" .escape_string($_GET['add'])."" );
+ confirm($query);
+
+ while($row = fetch_array($query)){
+
+ if($row['product_quantity'] != $_SESSION['product_' . $_GET['add']]){
+
+ $_SESSION['product_' . $_GET['add']]+=1;
+ redirect("checkout.php");
+
+
+ }else{
+
+  set_message("We only have " . $row['product_quantity']  . " " ."{$row['product_title']} " . "available");
+  redirect("checkout.php");
+
+ }
+
+
+ } 
+
+//$_SESSION['product_' . $_GET['add']] +=1;  
+
+//redirect("index.php");
+    
+}
+
+
+
+
+
+?>
