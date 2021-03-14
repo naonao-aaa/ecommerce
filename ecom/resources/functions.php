0763fddf74c915602973e345f76f7486ce6fe95a
Nao <naonao1814naonao@gmail.com>
2021-03-14 21:05:27 +0900

    製品編集ページ
    編集時に画像が選択されていなかったら元の画像を表示する機能、選択要素でのデフォルトカテゴリ名の表示機能など

--- ecom/resources/functions.php
+++ ecom/resources/functions.php
@@ -391,6 +391,60 @@ echo $category_options;
 }
 
 
+/************************updating product code***************/
+
+function update_product(){
+
+if(isset($_POST['update'])){
+
+$product_title          = escape_string($_POST['product_title']);
+$product_category_id    = escape_string($_POST['product_category_id']);
+$product_price          = escape_string($_POST['product_price']);
+$product_description    = escape_string($_POST['product_description']);
+$short_desc             = escape_string($_POST['short_desc']);
+$product_quantity       = escape_string($_POST['product_quantity']);
+$product_image          = $_FILES['file']['name'];
+$image_temp_location    = $_FILES['file']['tmp_name'];
+
+if(empty($product_image)){
+
+$get_pic = query("SELECT product_image FROM products WHERE product_id =" .escape_string($_GET['id']). " ");
+confirm($get_pic);
+
+while($pic = fetch_array($get_pic)){
+
+$product_image = $pic['product_image'];
+
+}
+
+}
+
+
+move_uploaded_file($image_temp_location, UPLOAD_DIRECTORY . DS . $product_image);
+
+$query = "UPDATE products SET ";
+$query .= "product_title       = '{$product_title}'        ,";
+$query .= "product_category_id = '{$product_category_id}'  ,";
+$query .= "product_price       = '{$product_price}'        ,";
+$query .= "product_description = '{$product_description}'  ,";
+$query .= "short_desc          = '{$short_desc}'           ,";
+$query .= "product_quantity    = '{$product_quantity}'     ,";
+$query .= "product_image       = '{$product_image}'         ";
+$query .= "WHERE product_id=" . escape_string($_GET['id']);
+
+$send_update_query = query($query);
+
+confirm($send_update_query);
+
+set_message("Product has been updated");
+
+redirect("index.php?products");
+
+}
+
+
+}
+
 
 
 
--- ecom/resources/templates/back/edit_product.php
+++ ecom/resources/templates/back/edit_product.php
@@ -1,10 +1,44 @@
 
+<?php
+
+
+if(isset($_GET['id'])){
+
+$query = query("SELECT * FROM products WHERE product_id = " . escape_string($_GET['id']) . "");
+confirm($query);
+
+while($row = fetch_array($query)){
+
+  $product_title          = escape_string($row['product_title']);
+  $product_category_id    = escape_string($row['product_category_id']);
+  $product_price          = escape_string($row['product_price']);
+  $product_description    = escape_string($row['product_description']);
+  $short_desc             = escape_string($row['short_desc']);
+  $product_quantity       = escape_string($row['product_quantity']);
+  $product_image          = $row['product_image'];
+
+  $product_image = display_image($row['product_image']);
+
+}
+
+
+update_product();
+
+}
+
+
+
+?>
+
+
+
+
 <div class="col-md-12">
 
 <div class="row">
 <h1 class="page-header">
    Edit Product
-
+  
 </h1>
 </div>
                
@@ -17,14 +51,14 @@
 
 <div class="form-group">
     <label for="product-title">Product Title </label>
-        <input type="text" name="product_title" class="form-control">
+        <input type="text" name="product_title" class="form-control" value="<?php echo $product_title; ?>">
        
     </div>
 
 
     <div class="form-group">
            <label for="product-title">Product Description</label>
-      <textarea name="product_description" id="" cols="30" rows="10" class="form-control"></textarea>
+      <textarea name="product_description" id="" cols="30" rows="10" class="form-control"><?php echo $product_description;?></textarea>
     </div>
 
 
@@ -32,9 +66,14 @@
     <div class="form-group row">
 
       <div class="col-xs-3">
-        <label for="product-price">Product Price</label>
-        <input type="number" name="product_price" class="form-control" size="60">
+        <label for="product-price">ProductPrice</label>
+        <input type="number" name="product_price" class="form-control" size="60" value="<?php echo $product_price; ?>" >
       </div>
+    </div>
+
+    <div class="form-group">
+           <label for="product-title">Product Short Description</label>
+      <textarea name="short_desc" id="" cols="30" rows="3" class="form-control"><?php echo $short_desc; ?></textarea>
     </div>
 
 
@@ -54,7 +93,7 @@
      
      <div class="form-group">
        <input type="submit" name="draft" class="btn btn-warning btn-lg" value="Draft">
-        <input type="submit" name="publish" class="btn btn-primary btn-lg" value="Publish">
+        <input type="submit" name="update" class="btn btn-primary btn-lg" value="Update">
     </div>
 
 
@@ -62,9 +101,11 @@
 
     <div class="form-group">
          <label for="product-title">Product Category</label>
-          <hr>
-        <select name="product_category" id="" class="form-control">
-            <option value="">Select Category</option>
+         
+        <select name="product_category_id" id="" class="form-control">
+            <option value="<?php echo $product_category_id; ?>"><?php echo show_product_category_title($product_category_id); ?></option>
+
+            <?php show_categories_add_product_page(); ?>
            
         </select>
 
@@ -79,26 +120,26 @@
 
 
     <div class="form-group">
-      <label for="product-title">Product Brand</label>
-         <select name="product_brand" id="" class="form-control">
-            <option value="">Select Brand</option>
-         </select>
+      <label for="product-title">Product Quantity</label>
+        <input type="number" name="product_quantity" class="form-control" value="<?php echo $product_quantity; ?>">
     </div>
 
 
 <!-- Product Tags -->
 
 
-    <div class="form-group">
+    <!--<div class="form-group">
           <label for="product-title">Product Keywords</label>
           <hr>
         <input type="text" name="product_tags" class="form-control">
-    </div>
+    </div>-->
 
     <!-- Product Image -->
     <div class="form-group">
         <label for="product-title">Product Image</label>
-        <input type="file" name="file">
+        <input type="file" name="file"> <br>
+
+        <img width='200' src="../../resources/<?php echo $product_image; ?>" alt="">
       
     </div>
 
@@ -109,33 +150,3 @@
 
     
 </form>
-
-
-
-                
-
-
-
-            </div>
-            <!-- /.container-fluid -->
-
-        </div>
-        <!-- /#page-wrapper -->
-
-    </div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
-
-</body>
-
-</html>
