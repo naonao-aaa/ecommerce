19e48560a790c0d81c939b7d6c90cf5cc034b804
nao <naonao1814naonao@gmail.com>
2021-04-21 11:35:25 +0900

    xampp→mammp
    PC買い替えに伴う

--- ecom/resources/config.php
+++ ecom/resources/config.php
@@ -15,7 +15,7 @@ defined("UPLOAD_DIRECTORY") ? null : define("UPLOAD_DIRECTORY",__DIR__ .DS. "upl
 
 defined("DB_HOST") ? null : define("DB_HOST","localhost");
 defined("DB_USER") ? null : define("DB_USER","root");
-defined("DB_PASS") ? null : define("DB_PASS","");
+defined("DB_PASS") ? null : define("DB_PASS","root");
 defined("DB_NAME") ? null : define("DB_NAME","ecom_db");
 
 $connection=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
